a0863f36267d1517c0b25ac0b7f51325d6f4de4b
Wiktor Wojcieski <126779051+WWiktori@users.noreply.github.com>
2024-03-18 18:20:35 +0200

    Add files via upload

---
+++ TaskServis.cs
@@ -0,0 +1,20 @@
+﻿using BlazorCalendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class TaskService
+{
+    public Task<List<TaskItem>> GetTasksAsync()
+    {
+        var tasks = new List<TaskItem>
+        {
+            new TaskItem { DueDate = DateTime.Today, Description = "Поприбирати кімнату" },
+            new TaskItem { DueDate = DateTime.Today, Description = "Приготувати вечерю" },
+            new TaskItem { DueDate = DateTime.Today, Description = "Зробити зарядку" },
+            // Додайте інші завдання за бажанням
+        };
+
+        return Task.FromResult(tasks);
+    }
+}
